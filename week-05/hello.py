32d0d217287cc8c7a48080da6813b9270fa8c1d2
elizabeas <bess@rcc.team>
2026-03-15 22:43:28 -0500

    Module 1 python exploratory exercises

--- week-05/hello.py
+++ week-05/hello.py
@@ -1,1 +1,12 @@
 print("Hello world!")
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world" prints twice because ...
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = .50
+print(dollars + cents)
