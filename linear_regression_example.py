bd5bff3e7ca13f54a9421bf8032e3710b9395145
elizabeas <bess@rcc.team>
2026-03-18 22:09:15 -0500

    added print output to end of script

--- linear_regression_example.py
+++ linear_regression_example.py
@@ -23,3 +23,5 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Step 3: Create the model and fit it to training data
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+print('Model trained successfully.')
